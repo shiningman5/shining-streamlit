b6189af0bd1b50724a956e46f39bc00a28e5bace
shiningman5 <100816644+shiningman5@users.noreply.github.com>
2022-03-03 17:31:01 +0900

    1stcommit

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-from turtle import left
 import streamlit as st 
-import numpy as np
-import pandas as pd
-from PIL import Image
 import time
 
 st.title("Streamlit 超入門")
@@ -32,25 +28,5 @@ expander2 = st.expander("問い合わせ2")
 expander2.write("問い合わせ2の回答")
 expander3 = st.expander("問い合わせ3")
 expander3.write("問い合わせ3の回答")
-
-
-
-
-
-    
-
-
-
-# text = st.text_input( "あなたの趣味を教えて下さい。")
-# condistion = st.slider("あなたの今の調子は？", 0, 100, 50)
-
-
-# "あなたの趣味: ", text
-# "コンディション:", condistion
-
-
-
-
-
 
 
